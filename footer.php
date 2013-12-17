4d7f5f3363c0af65fb5410e43431e4d7bb4a6d95
Marc Juchli <marc@mjnet.ch>
2013-12-16 21:34:14 -0800

    fixed more broken links and added sticky footer

--- footer.php
+++ footer.php
@@ -10,10 +10,10 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="wp-content/themes/wordpress-bootstrap-master/library/bounce/js/jquery-1.8.3.min.js"><\/script>')</script>
-    <script src="wp-content/themes/wordpress-bootstrap-master/library/bounce/js/bootstrap.min.js"></script>
-	<script src="wp-content/themes/wordpress-bootstrap-master/library/bounce/js/jquery.fitvids.js"></script>
-	<script src="wp-content/themes/wordpress-bootstrap-master/library/bounce/js/jqBootstrapValidation.js"></script>
-	<script src="wp-content/themes/wordpress-bootstrap-master/library/bounce/js/bounce.js"></script>
+    <script src="/wp-content/themes/<?php echo get_template(); ?>/library/bounce/js/bootstrap.min.js"></script>
+	<script src="/wp-content/themes/<?php echo get_template(); ?>/library/bounce/js/jquery.fitvids.js"></script>
+	<script src="/wp-content/themes/<?php echo get_template(); ?>/library/bounce/js/jqBootstrapValidation.js"></script>
+	<script src="/wp-content/themes/<?php echo get_template(); ?>/library/bounce/js/bounce.js"></script>
 
   </body>
 </html>
